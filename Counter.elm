ac6341a2b179968bb08fd879236a073db51d5606
achernyak <artemchernyak@gmail.com>
2016-02-11 18:50:25 -0600

    counter

---
+++ Counter.elm
@@ -0,0 +1,61 @@
+module Counter where
+
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Graphics.Input as Input
+
+
+-- MODEL
+
+type alias Model = Int
+
+
+-- UPDATE
+
+type Action = Increment | Decrement
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div [containerStyle]
+    [ button [ onClick address Decrement, buttonStyle] [ text "-" ]
+    , div [ countStyle ] [ text (toString model) ]
+    , button [ onClick address Increment, buttonStyle] [ text "+" ]
+    ]
+
+
+containerStyle : Attribute
+containerStyle =
+  style
+    [ ("margin", "1em")]
+
+countStyle : Attribute
+countStyle =
+  style
+    [ ("font-size", "1.5em")
+    , ("font-family", "monospace")
+    , ("display", "inline-block")
+    , ("width", "2em")
+    , ("text-align", "center")
+    ]
+
+buttonStyle : Attribute
+buttonStyle =
+  style
+    [ ("font-size", "2em")
+    , ("width", "2em")
+    , ("background", "white")
+    , ("border", "2px solid #DDD")
+    ]
